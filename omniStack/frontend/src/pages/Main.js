689ff89ecb36e2c18e61cbbdcef05e48507f6c5d
Alan Barros <alan.barros@outlook.com>
2019-08-11 16:37:49 -0300

    Conclusão tindev - WebSocket realtime

--- omniStack/frontend/src/pages/Main.js
+++ omniStack/frontend/src/pages/Main.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Main.css';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
+
 import api from '../services/api';
 
 export default function Main({ match }){
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadUsers(){
@@ -24,12 +28,23 @@ export default function Main({ match }){
         loadUsers();
     }, [match.params.id]);
 
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        });
+
+    }, [match.params.id]);
+
     async function handleLike(id){
         await api.post(`/devs/${id}/likes`, null, {
             headers: { user: match.params.id },
         })
 
-        setUsers(users.filter(user => user._id != id));
+        setUsers(users.filter(user => user._id !== id));
     }
 
     async function handleDislike(id){
@@ -37,7 +52,7 @@ export default function Main({ match }){
             headers: { user: match.params.id },
         })
 
-        setUsers(users.filter(user => user._id != id));
+        setUsers(users.filter(user => user._id !== id));
     }
     
     return (
@@ -68,6 +83,16 @@ export default function Main({ match }){
             ) : (
                 <div className="empty">Ababou :(</div>
             )}
+            { matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a Match"/>
+                    <img className="avatar" src={matchDev.avatar} alt={matchDev.name}/>
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+                    <button type='button' onClick={() => setMatchDev(null)}>FECHAR</button>
+                </div>
+            )}
+
         </div>
     )
 }
--- omniStack/mobile/src/pages/Main.js
+++ omniStack/mobile/src/pages/Main.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import AsyncStorage from '@react-native-community/async-storage';
+import io from 'socket.io-client';
 
 import logo from '../assets/logo.png';
 import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
+import itsamatch from '../assets/itsamatch.png';
 
 import api from '../services/api';
 
 export default function Main({ navigation }) {
     const id = navigation.getParam('user');
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadUsers(){
@@ -20,20 +23,25 @@ export default function Main({ navigation }) {
                     user: id,
                 }
             })
-
             setUsers(response.data);
         }
-
         loadUsers();
+    }, [id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: id }
+        });
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        });
     }, [id]);
 
     async function handleLike(){
         const [user, ...rest] = users;
-
         await api.post(`/devs/${user._id}/likes`, null, {
             headers: { user: id },
-        })
-
+        });
         setUsers(rest);
     }
 
@@ -82,6 +90,17 @@ export default function Main({ navigation }) {
                     <Image source={like} />
                 </TouchableOpacity>
             </View>
+            )}
+            {matchDev && (
+                <View style={styles.matchContainer}>
+                    <Image style={styles.matchImage} source={itsamatch}/>
+                    <Image style={styles.matchAvatar} source={{uri: matchDev.avatar }}/>
+                    <Text style={styles.matchName}>{matchDev.name}</Text>
+                    <Text style={styles.matchBio}>{matchDev.bio}</Text>
+                    <TouchableOpacity onPress={() => setMatchDev(null)}>
+                        <Text style={styles.matchClose}>FECHAR</Text>
+                    </TouchableOpacity>
+                </View>
             )}
         </SafeAreaView>
     );
@@ -161,5 +180,43 @@ const styles = StyleSheet.create({
             width: 0,
             height: 2,
         },
+    },
+    matchContainer: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    matchImage: {
+        height: 60,
+        resizeMode: 'contain',
+    },
+    matchAvatar: {
+        width: 160,
+        height: 160,
+        borderRadius: 80,
+        borderWidth: 5,
+        borderColor: '#FFF',
+        marginVertical: 30,
+    },
+    matchName: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: '#FFF',
+    },
+    matchBio: {
+        marginTop: 10,
+        fontSize: 16,
+        color: 'rgba(255,255,255,0.8)',
+        lineHeight: 24,
+        textAlign: 'center',
+        paddingHorizontal: 30,
+    },
+    matchClose: {
+        fontSize: 16,
+        color: 'rgba(255,255,255,0.8)',
+        textAlign: 'center',
+        marginTop: 30,
+        fontWeight: 'bold',
     },
 });
